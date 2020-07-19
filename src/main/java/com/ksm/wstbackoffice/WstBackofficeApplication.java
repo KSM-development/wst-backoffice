8eb8628c5370f6288818b86dcefcf3680a5d2fd5
SerjiK <sergiykrohmalniy@gmail.com>
2020-07-19 22:17:44 +0300

    init project

---
+++ src/main/java/com/ksm/wstbackoffice/WstBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.ksm.wstbackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WstBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksm/wstbackoffice/WstBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksm.wstbackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
