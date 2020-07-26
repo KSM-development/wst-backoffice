f3a80517128d24d23f8e52b0ca52f37e0d1b3654
SerjiK <sergiykrohmalniy@gmail.com>
2020-07-27 00:51:13 +0300

    CRUD country

---
+++ src/main/java/com/ksm/wstbackoffice/service/CountryService.java
@@ -0,0 +1,19 @@
+package com.ksm.wstbackoffice.service;
+
+import com.ksm.wstbackoffice.entity.Country;
+import com.ksm.wstbackoffice.repository.CountryRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CountryService {
+    private CountryRepository countryRepository;
+
+    public CountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    public List<Country> findAll() {
+        return countryRepository.findAll();
+    }
+}
