b99a989ce0e25c2dcb9f143dcd2261da61a02242
SerjiK <sergiykrohmalniy@gmail.com>
2020-07-27 13:04:21 +0300

    fixed crud country

--- src/main/java/com/ksm/wstbackoffice/repository/CountryRepository.java
+++ src/main/java/com/ksm/wstbackoffice/repository/CountryRepository.java
@@ -3,5 +3,5 @@ package com.ksm.wstbackoffice.repository;
 import com.ksm.wstbackoffice.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CountryRepository extends JpaRepository<Country, Long> {
+public interface CountryRepository extends JpaRepository<Country, String> {
 }
