241163d18fe9ade065371b51206d07a103f50b99
Oleg Pinta <oleh.pinta@adidas.com>
2020-08-07 12:45:26 +0300

    test

--- src/test/java/com/ksm/wstbackoffice/WstBackofficeApplicationTests.java
+++ src/test/java/com/ksm/wstbackoffice/WstBackofficeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.autoconfigure.exclude=com.integralblue.log4jdbc.spring.Log4jdbcAutoConfiguration"})
 @ActiveProfiles("test")
 class WstBackofficeApplicationTests {
 
